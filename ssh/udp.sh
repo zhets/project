f2d871b0a9a1799edb35fbc49c81f5d3d169ee12
zhets <142297155+zhets@users.noreply.github.com>
2023-12-14 15:11:58 +0700

    Update udp.sh

--- ssh/udp.sh
+++ ssh/udp.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-clear
 cd
-rm -rf /root/udp
-mkdir -p /root/udp
+rm -rf /etc/udp
+mkdir -p /etc/udp
 
 # change to time GMT+7
 echo "change to time GMT+7"
@@ -10,25 +9,23 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # install udp-custom
 echo downloading udp-custom
-cd /root/udp
 wget -q -O udp-custom "https://raw.githubusercontent.com/zhets/project/main/ssh/udp-custom-linux-amd64"
-chmod +x udp-custom
-sleep 2
+chmod +x /etc/udp/udp-custom
+
 echo downloading default config
 wget -q -O config.json "https://raw.githubusercontent.com/zhets/project/main/ssh/config.json"
-chmod 644 config.json
-cd
+chmod 644 /etc/udp/config.json
 
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom
+Description=UDP Custom by ePro Dev. Team
 
 [Service]
 User=root
 Type=simple
-ExecStart=/root/udp/udp-custom server
-WorkingDirectory=/root/udp/
+ExecStart=/etc/udp/udp-custom server
+WorkingDirectory=/etc/udp/
 Restart=always
 RestartSec=2s
 
@@ -38,13 +35,13 @@ EOF
 else
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom
+Description=UDP Custom by ePro Dev. Team
 
 [Service]
 User=root
 Type=simple
-ExecStart=/root/udp/udp-custom server -exclude $1
-WorkingDirectory=/root/udp/
+ExecStart=/etc/udp/udp-custom server -exclude $1
+WorkingDirectory=/etc/udp/
 Restart=always
 RestartSec=2s
 
@@ -53,7 +50,11 @@ WantedBy=default.target
 EOF
 fi
 
+echo start service udp-custom
 systemctl start udp-custom &>/dev/null
+
+echo enable service udp-custom
 systemctl enable udp-custom &>/dev/null
+
+echo restart service udp-custom
 systemctl restart udp-custom &>/dev/null
-rm -rf udp.sh
