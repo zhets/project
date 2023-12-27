f5baec2b02277fd804202bc3ce8204b318e58e0a
XDXL VPN STORES <142297155+zhets@users.noreply.github.com>
2023-12-27 17:58:42 +0700

    Update ins-xdxl.sh

--- project-xdxl/ins-xdxl.sh
+++ project-xdxl/ins-xdxl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 REPO="https://raw.githubusercontent.com/zhets/project/main/project-xdxl/"
-wget -O /usr/bin/c-vl "${REPO}c-vl
+wget -O /usr/bin/c-vl "${REPO}c-vl"
 chmod +x /usr/bin/c-vl
 wget -O /etc/nginx/conf.d/xray.conf "${REPO}xray.conf" >/dev/null 2>&1
 wget -O /etc/xray/config.json "${REPO}config.json" >/dev/null 2>&1
-￼systemctl restart xray
+￼ systemctl restart xray
 ckrun
